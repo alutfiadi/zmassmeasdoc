02df760fac1b8447de0f4cd064a551c9395ac62c
alutfiadi <ahmadlutfiadi@gmail.com>
2023-10-05 09:20:22 +0700

    Change Odata measurement point

--- webapp/controller/Create.controller.js
+++ webapp/controller/Create.controller.js
@@ -42,68 +42,111 @@ sap.ui.define(
         );
 
         //PASSED DATA
-        // console.log("PASSED", this.getOwnerComponent().getModel("PassModel"));
         var passedData = this.getOwnerComponent()
           .getModel("PassModel")
           .getProperty("/rentalObject");
         if (typeof passedData == "undefined") {
           this.getRouter().navTo("RouteHome");
         }
+        this.byId("EquipmentId").setValue(
+          this.equipmentname + " (" + passedData.Equipment + ")"
+        );
+
+        //create filter by equipment id
         let aFilter = new Filter(
           "Equipment",
           sap.ui.model.FilterOperator.EQ,
           this.equipmentid
         );
 
-        let oList = oModelPoint.bindList(
-          "/MeasuringPoint",
-          undefined,
-          undefined,
-          aFilter,
-          undefined
-        );
-
-        this.byId("EquipmentId").setValue(
-          this.equipmentname + " (" + passedData.Equipment + ")"
-        );
-
-        this.mPoints = [];
-        oList.requestContexts(0, 100).then(function(aContexts) {
-          if (aContexts.length == 0) {
-            // console.log("NO DATA");
-            // No Measuring Point
-            MessageBox.information(
-              "No Measuring Points for " + that.equipmentid,
-              {
-                actions: [MessageBox.Action.OK],
-                emphasizedAction: MessageBox.Action.OK,
-                onClose: function(sAction) {
-                  that.getRouter().navTo("RouteHome");
+        this.mPoints = []; // Array for component ID
+        oModelPoint.read("/ZC_AL_MEASURINGPOINTS", {
+          filters: [aFilter],
+          success: function(oData, oResponse) {
+            console.log(oData);
+            let results = oData.results;
+            if (results.length == 0) {
+              // console.log("NO DATA");
+              // No Measuring Point
+              MessageBox.information(
+                "No Measuring Points for " + that.equipmentid,
+                {
+                  actions: [MessageBox.Action.OK],
+                  emphasizedAction: MessageBox.Action.OK,
+                  onClose: function(sAction) {
+                    that.getRouter().navTo("RouteHome");
+                  }
                 }
-              }
-            );
-          } else {
-            aContexts.forEach(function(oContext) {
-              // As we have fetched the data already, we build panel from emasruing pints data
-              that.createPanel(oContext);
-            });
+              );
+            } else {
+              results.forEach(function(oContext) {
+                // As we have fetched the data already, we build panel from emasruing pints data
+                that.createPanel(oContext);
+              });
+            }
+          },
+          error: function(oError) {
+            console.log(oError);
           }
         });
+
+        // Code for Odata V4
+        // let oList = oModelPoint.bindList(
+        //   "/ZC_AL_MEASURINGPOINTS",
+        //   undefined,
+        //   undefined,
+        //   aFilter,
+        //   undefined
+        // );
+
+        // oList.requestContexts(0, 100).then(function(aContexts) {
+        //   if (aContexts.length == 0) {
+        //     // console.log("NO DATA");
+        //     // No Measuring Point
+        //     MessageBox.information(
+        //       "No Measuring Points for " + that.equipmentid,
+        //       {
+        //         actions: [MessageBox.Action.OK],
+        //         emphasizedAction: MessageBox.Action.OK,
+        //         onClose: function(sAction) {
+        //           that.getRouter().navTo("RouteHome");
+        //         }
+        //       }
+        //     );
+        //   } else {
+        //     aContexts.forEach(function(oContext) {
+        //       // As we have fetched the data already, we build panel from emasruing pints data
+        //       that.createPanel(oContext);
+        //     });
+        //   }
+        // });
       },
 
       createPanel: function(oItem) {
+        console.log(oItem);
         let oPage = this.getView().byId("pageCreate");
         let MeasuringPoint = oItem.getProperty("MeasuringPoint");
         let MeasuringPointDescription = oItem.getProperty(
           "MeasuringPointDescription"
         );
         let CharcValueUnit = oItem.getProperty("CharcValueUnit");
+        let MeasuringPointIsCounter = oItem.getProperty(
+          "MeasuringPointIsCounter"
+        );
+        let ValuationCodeIsSufficient = oItem.getProperty(
+          "ValuationCodeIsSufficient"
+        );
+
         let today = new Date();
         let read_by = sap.ushell.Container.getService("UserInfo").getId();
         let equipment = oItem.getProperty("Equipment");
 
-        // Create a panel for each item
+        // Define varibale for each component ID
         let panelId = this.createId("panel" + MeasuringPoint);
+        let ireadingId, idifferenceId, itargetId;
+
+        // Create a panel for each item
+
         var oPanel = new Panel(panelId, {
           headerText: MeasuringPoint + " - " + MeasuringPointDescription,
           expandable: true,
@@ -119,19 +162,41 @@ sap.ui.define(
         });
         oInput1.addStyleClass("sapUiTinyMarginBottom");
 
-        let ireadingId = this.createId("reading" + MeasuringPoint);
-        var oLabel2 = new Label({ text: "Reading" });
-        var oInput2 = new Input(ireadingId, {
-          description: CharcValueUnit
-        });
-        oInput2.addStyleClass("sapUiTinyMarginBottom");
+        // Check for Measuring Point type
+        if (MeasuringPointIsCounter && !ValuationCodeIsSufficient) {
+          // console.log("Input REading and Difference");
+          ireadingId = this.createId("reading" + MeasuringPoint);
+          var oLabel2 = new Label({ text: "Reading" });
+          var oInput2 = new Input(ireadingId, {
+            description: CharcValueUnit
+          });
+          oInput2.addStyleClass("sapUiTinyMarginBottom");
 
-        let idifferenceId = this.createId("difference" + MeasuringPoint);
-        var oLabel3 = new Label({ text: "Difference" });
-        var oInput3 = new Input(idifferenceId, {
-          description: CharcValueUnit
-        });
-        oInput3.addStyleClass("sapUiTinyMarginBottom");
+          idifferenceId = this.createId("difference" + MeasuringPoint);
+          var oLabel3 = new Label({ text: "Difference" });
+          var oInput3 = new Input(idifferenceId, {
+            description: CharcValueUnit
+          });
+          oInput3.addStyleClass("sapUiTinyMarginBottom");
+        } else if (!MeasuringPointIsCounter && !ValuationCodeIsSufficient) {
+          // console.log("Input REading");
+          ireadingId = this.createId("reading" + MeasuringPoint);
+          var oLabel2 = new Label({ text: "Reading" });
+          var oInput2 = new Input(ireadingId, {
+            description: CharcValueUnit
+          });
+          oInput2.addStyleClass("sapUiTinyMarginBottom");
+
+          itargetId = this.createId("target" + MeasuringPoint);
+          var oLabel3 = new Label({ text: "Target" });
+          var oInput3 = new Input(itargetId, {
+            value: oItem.getProperty("MeasuringPointTargetValue"),
+            description: CharcValueUnit
+          });
+          oInput3.addStyleClass("sapUiTinyMarginBottom");
+        } else if (!MeasuringPointIsCounter && ValuationCodeIsSufficient) {
+          // console.log("Input Valuation");
+        }
 
         let idatetimeId = this.createId("datetime" + MeasuringPoint);
         var oLabel4 = new Label({ text: "Date Time" });
@@ -176,11 +241,43 @@ sap.ui.define(
           smeasuringpoint: MeasuringPoint,
           spanelId: panelId,
           sreadingId: ireadingId,
+          stargetId: itargetId,
           sdifferenceId: idifferenceId,
           sdatetimeId: idatetimeId,
           sreadbyId: ireadbyId,
           stextId: itextId
         });
+
+        // //GET LAST Value of Measurement Doc by Measurement Point
+        // var oModel = this.getOwnerComponent().getModel("measurementdocument");
+        // var oDoc = oModel.bindList("/MeasurementDocument");
+        // var sFilter = "(MeasuringPoint eq '" + MeasuringPoint + "')";
+        // oDoc.changeParameters({
+        //   $select:
+        //     "MeasurementDocument,MeasuringPoint,MeasurementReading,MeasurementCounterReading,MsmtCounterReadingDifference,MeasurementReadingInEntryUoM,MsmtDocumentSIUnitOfMeasure,MsmtRdngDate,MsmtRdngTime",
+        //   $filter: sFilter,
+        //   $orderby:
+        //     "MeasuringPoint,MeasurementDocument desc,MsmtRdngDate desc,MsmtRdngTime desc"
+        // });
+        // oDoc.requestContexts(0, 1).then(function(aContexts) {
+        //   aContexts.forEach(function(oContext) {
+        //     // As we have fetched the data already, we can access "Note" through getProperty
+        //     var TotalDiff = oContext.getProperty(
+        //       "MeasurementReadingInEntryUoM"
+        //     );
+        //     var lastdatetime =
+        //       oContext.getProperty("MsmtRdngDate") +
+        //       " " +
+        //       oContext.getProperty("MsmtRdngTime");
+        //     var lastdatetime = new Date(lastdatetime);
+        //     // console.log(sNote);
+        //     // that.byId("TotalCounter").setValue(TotalDiff);
+        //     // that.byId("lastValue").setValue(TotalDiff);
+        //     // that.byId("LastMsmtRdngDate").setValue(lastdatetime);
+        //     console.log("TOTAL DIFF: " + TotalDiff);
+        //     console.log("Last Measurement time: " + lastdatetime);
+        //   });
+        // });
       },
       onSave: function() {
         let sinfotext = "";
